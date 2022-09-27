eda0406d31d1d2cb0e840a13855b52b52b63b6f6
Glivestyles10 <glivegijinessc@gmail.com>
2022-09-27 03:49:26 -0700

    new styles are add

--- src/Components/ExperirenceContainer/ExpereniceContainer.js
+++ src/Components/ExperirenceContainer/ExpereniceContainer.js
@@ -5,13 +5,13 @@ function Experience(){
         <div className="experience" id="exp">
             <div  className="achivements">
                 <div className="circle">3+</div>
-                <span color="orange">months</span>
-                <span>FrontEnd Dev</span>
+                <span className="line1"> Months</span>
+                <h4 className="line2"> FrontEnd Dev</h4>
             </div>
             <div className="achivements">
                 <div className="circle">3+</div>
-                <span>Html, Css ,Js , React js</span>
-                <span>self Experience</span>
+                <span className="line3">Html, Css ,Js , React js</span>
+                <h1 className="line4"> self Experience</h1>
             </div>
         </div>
 
